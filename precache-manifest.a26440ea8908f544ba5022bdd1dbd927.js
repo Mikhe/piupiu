0e11b33463dc28a295be81c78c9cb859859957b5
Sergei Mikhailovskikh <s.mikhailovskikh@volsor.com>
2019-07-06 09:43:03 +0200

    Updates

---
+++ precache-manifest.a26440ea8908f544ba5022bdd1dbd927.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8110b11619d0c6aac5e276735207e338",
+    "url": "/piupiu/index.html"
+  },
+  {
+    "revision": "9cd9eb0b98db6217abc8",
+    "url": "/piupiu/static/css/main.84528ac9.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/piupiu/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "9cd9eb0b98db6217abc8",
+    "url": "/piupiu/static/js/main.ac60ce99.chunk.js"
+  },
+  {
+    "revision": "3e1ad954a2d8b8dfd618",
+    "url": "/piupiu/static/js/runtime~main.15cc0b4a.js"
+  }
+]);
